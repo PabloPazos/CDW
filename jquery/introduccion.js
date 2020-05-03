820937cf2e3774564272f777b6014ed94e91f0d0
PabloPazos <pablopazos_lp@hotmail.com>
2020-05-02 21:02:47 -0300

    Sección de jquery

---
+++ jquery/introduccion.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+    //$(selector).accion();
+    
+    /*
+        $ - Indica que vamos a acceder a jquery.
+        selector - accedemos al elemento que vamos a trabajar.
+        accion - lo que se quiere hacer con el elemento seleccionado.
+    */
+
+    $('h1').hide();
+});
+
+//-----------
+
+$(function(){
+
+});
